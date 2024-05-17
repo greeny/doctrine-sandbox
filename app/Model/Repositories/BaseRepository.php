8a35e566601e773e8d0b4b5bf2622080d90816a8
Tomáš Blatný <blatny.tomas@seznam.cz>
2024-05-17 09:56:34 +0200

    Small update

--- app/Model/Repositories/BaseRepository.php
+++ app/Model/Repositories/BaseRepository.php
@@ -18,13 +18,19 @@ abstract class BaseRepository
 
 	public function save(object $entity, bool $flush = TRUE): void
 	{
-		$className = $this->getEntityClass();
-
-		if (!($entity instanceof $className)) {
-			throw new InvalidArgumentException('Entity ' . get_class($entity) . ' does not belong to ' . get_class($this));
-		}
+		$this->checkOwnership($entity);
 
 		$this->entityManager->persist($entity);
+		if ($flush) {
+			$this->entityManager->flush($entity);
+		}
+	}
+
+	public function delete(object $entity, bool $flush = TRUE): void
+	{
+		$this->checkOwnership($entity);
+
+		$this->entityManager->remove($entity);
 		if ($flush) {
 			$this->entityManager->flush($entity);
 		}
@@ -33,6 +39,15 @@ abstract class BaseRepository
 	protected function getRepository(): EntityRepository
 	{
 		return $this->entityManager->getRepository($this->getEntityClass());
+	}
+
+	protected function checkOwnership(object $entity): void
+	{
+		$className = $this->getEntityClass();
+
+		if (!($entity instanceof $className)) {
+			throw new InvalidArgumentException('Entity ' . get_class($entity) . ' does not belong to ' . get_class($this));
+		}
 	}
 
 	abstract protected function getEntityClass(): string;
